38e0518971ff8911fe5db3cfbac6485c82af100a
duy manh <duym171003@gmail.com>
2023-10-20 22:07:18 +0700

    Release site

--- chatcore/name-generator/name_generator.py
+++
@@ -1,7 +0,0 @@
-with open("Name", "r") as file:
-    lines = file.readlines()
-    list = [line.strip() for line in lines]
-
-with open("Ten", "w") as file:
-    for x in list:
-        file.write(f"- Tôi là [{x}](\"entity\": \"cust_name\").\n")
